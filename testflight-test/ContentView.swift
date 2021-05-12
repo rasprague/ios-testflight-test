2cc1724df08b94f3bcb5cc6a005ffd9900d35c3f
Richard Sprague <rasprague@gmail.com>
2021-05-11 21:26:40 -0700

    initial repo population

---
+++ testflight-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testflight-test
+//
+//  Created by spike on 5/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testflight-test/testflight_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testflight_testApp.swift
+//  testflight-test
+//
+//  Created by spike on 5/11/21.
+//
+
+import SwiftUI
+
+@main
+struct testflight_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
